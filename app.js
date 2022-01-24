124b01d0df71c41f7e7ec57aecb685d48167f44d
Gaywin <gaywin@gmail.com>
2022-01-24 19:38:51 +0200

    update app.js
    adding todos

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 
 
-//get reference to form
-
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
 
+const generateTemplate = todo => {
+
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+
+  list.innerHTML += html;
+};
+
 addForm.addEventListener('submit', e => {
+
   e.preventDefault();
-  const todo = addForm.add.value
-  console.log(todo)
-})
+  const todo = addForm.add.value.trim();
+
+  generateTemplate(todo);
+});
+
